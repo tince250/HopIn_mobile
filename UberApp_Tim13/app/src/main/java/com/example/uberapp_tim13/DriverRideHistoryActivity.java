9ac9c898e4c79172485d445cd9a5dec2f923c823
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-11-12 01:31:15 +0100

    added view of past ride

---
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/DriverRideHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.uberapp_tim13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverRideHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_ride_history);
+    }
+}
