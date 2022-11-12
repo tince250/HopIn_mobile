776ab90b22f60d133720e780e4f40338cef7def8
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-11-12 17:37:54 +0100

    added ride offer card with hardcoded strings and temporary icons

---
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/AcceptanceRideActivity.java
@@ -0,0 +1,15 @@
+package com.example.uberapp_tim13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcceptanceRideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Ride offer");
+        setContentView(R.layout.activity_acceptance_ride);
+    }
+}
