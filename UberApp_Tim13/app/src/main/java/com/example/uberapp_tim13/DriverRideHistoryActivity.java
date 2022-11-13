7bc38f6e13e892cb553f08878401ebc895915d7b
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-11-13 23:33:37 +0100

    reorganized design

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/DriverRideHistoryActivity.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/DriverRideHistoryActivity.java
@@ -9,6 +9,6 @@ public class DriverRideHistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_ride_history);
+        setContentView(R.layout.driver_history_list);
     }
 }
