28d46fb420c4e1e5a9129a865c9aae8f5094e35a
tince250 <t.s.mihajlovic25@gmail.com>
2022-11-14 22:14:02 +0100

    driver history mockap now working, passenger pending

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/RideHistoryDetailsFragment.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/RideHistoryDetailsFragment.java
@@ -4,42 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.uberapp_tim13.R;
+import com.example.uberapp_tim13.items.RideItem;
 import com.example.uberapp_tim13.tools.FragmentTransition;
 import com.example.uberapp_tim13.tools.Globals;
+import com.example.uberapp_tim13.tools.Mockap;
 
 public class RideHistoryDetailsFragment extends Fragment{
-    public static RideHistoryDetailsFragment newInstance() {
-        return new RideHistoryDetailsFragment();
+    public static RideHistoryDetailsFragment newInstance(int ride_num) {
+        RideHistoryDetailsFragment frag = new RideHistoryDetailsFragment();
+        Bundle args = new Bundle();
+        args.putInt("ride_id", ride_num);
+        frag.setArguments(args);
+        return frag;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ride_history_details, container, false);
-        fitFragmentToRole(view);
+
         FragmentTransition.to(RideReviewsFragment.newInstance(), getActivity(), false, R.id.listViewReviews);
+
+        RideItem ride = Mockap.getRides().get(getArguments().getInt("ride_id"));
+//        fitFragmentToRole(view, ride);
+
+
+        ((TextView)view.findViewById(R.id.textViewRoute)).setText(ride.getPickUpLocation()+" -> "+ride.getDestination());
+        String stops = "";
+        for (String s : ride.getStops()){
+            stops = stops + s +", ";
+        }
+        ((TextView)view.findViewById(R.id.textViewStops)).setText("Stops: "+ stops);
+        ((TextView)view.findViewById(R.id.textViewStartTime)).setText(ride.getStartTime());
+        ((TextView)view.findViewById(R.id.textViewEndTime)).setText(ride.getEndTime());
+        ((TextView)view.findViewById(R.id.textViewPassengers)).setText("Passengers: " + ride.getPassengers().size());
+        ((TextView)view.findViewById(R.id.textViewPrice)).setText("Price(RSD): " + ride.getPrice());
 
         /*TextView tt = view.findViewById(R.id.textViewDistance);
         String s = getString(R.string.acceptanceRideDistance, "sss");
         tt.setText(s);*/
-        view.findViewById(R.id.passengerDetails).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransition.to(PassengerDetailsFragment.newInstance(), getActivity(), true, R.id.rideHistory);
-            }
-        });
+//        view.findViewById(R.id.passengerDetails).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FragmentTransition.to(PassengerDetailsFragment.newInstance(), getActivity(), true, R.id.list);
+//            }
+//        });
         return view;
     }
 
-    private void fitFragmentToRole(View view) {
+    private void fitFragmentToRole(View view, RideItem ride) {
         switch (Globals.userRole) {
             case "driver":
                 view.findViewById(R.id.driver_info_card_hist).setVisibility(View.GONE);
                 break;
             case "passenger":
+                ((TextView)view.findViewById(R.id.driver_info_card_hist)).setText("Driver: " + ride.getDriver().getName() + " " + ride.getDriver().getSurName());
                 break;
         }
     }
--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/RideHistoryFragment.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/RideHistoryFragment.java
@@ -1,21 +1,21 @@
 package com.example.uberapp_tim13.fragments;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
 
 import com.example.uberapp_tim13.R;
-import com.example.uberapp_tim13.items.InboxAdapter;
-import com.example.uberapp_tim13.items.RideHistoryAdapter;
+import com.example.uberapp_tim13.items.RideItem;
+import com.example.uberapp_tim13.items.ride_history.HistoryAdapter;
 import com.example.uberapp_tim13.tools.FragmentTransition;
 import com.example.uberapp_tim13.tools.Mockap;
 
-public class RideHistoryFragment extends Fragment{
-    RecyclerView recyclerView;
+public class RideHistoryFragment extends ListFragment{
     public static RideHistoryFragment newInstance() {
         return new RideHistoryFragment();
     }
@@ -24,10 +24,23 @@ public class RideHistoryFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ride_history, container, false);
 
-        recyclerView = view.findViewById(R.id.rideHistory);
-        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        recyclerView.setAdapter(new RideHistoryAdapter(view.getContext(), Mockap.getRides()));
-
         return view;
     }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        FragmentTransition.to(RideHistoryDetailsFragment.newInstance(position), getActivity(), true, R.id.driver_fl);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        HistoryAdapter adapter = new HistoryAdapter(getActivity());
+        setListAdapter(adapter);
+    }
+
+
 }
