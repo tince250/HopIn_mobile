7fdd3ee93aae3387b83c0cdfc01deff826e4b496
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-11-24 15:28:05 +0100

    added fragment transition between home and ride settings

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/RideSettingsFragment.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/RideSettingsFragment.java
@@ -1,6 +1,5 @@
 package com.example.uberapp_tim13.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,24 +8,23 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.uberapp_tim13.R;
-import com.example.uberapp_tim13.activities.AccountSettingsActivity;
 import com.example.uberapp_tim13.tools.FragmentTransition;
 
-public class PassengerHomeFragment extends Fragment {
+public class RideSettingsFragment extends Fragment {
 
-    public static PassengerHomeFragment newInstance() {
-        return new PassengerHomeFragment();
+    public static RideSettingsFragment newInstance() {
+        return new RideSettingsFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_passenger_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_ride_settings, container, false);
 
-        view.findViewById(R.id.nextBtn).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.backBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransition.to(RideSettingsFragment.newInstance(), getActivity(), true, R.id.passengerFL);
+                FragmentTransition.to(PassengerHomeFragment.newInstance(), getActivity(), true, R.id.passengerFL);
             }
         });
 
