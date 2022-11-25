f98cf2254553cb9fac5fda0fc2d76ec8a529db7f
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-11-25 15:27:14 +0100

    fixed bugs in displaying list view

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/InviteOthersFragment.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/InviteOthersFragment.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
 import com.example.uberapp_tim13.R;
@@ -18,7 +20,7 @@ import com.example.uberapp_tim13.tools.Mockap;
 import java.util.ArrayList;
 import java.util.List;
 
-public class InviteOthersFragment extends ListFragment {
+public class InviteOthersFragment extends Fragment implements View.OnClickListener {
     public static InviteOthersFragment newInstance() {
         return new InviteOthersFragment();
     }
@@ -26,38 +28,52 @@ public class InviteOthersFragment extends ListFragment {
     List<User> addedUsers;
     ImageView inviteBtn;
     InvitedPassengersAdapter adapter;
+    private ListView listView;
+    TextView emailTV;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_invite_others, container, false);
 
+        this.addedUsers = new ArrayList<User>();
+        this.adapter = new InvitedPassengersAdapter(getActivity(), this.addedUsers);
+        listView = (ListView) view.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
         inviteBtn = (ImageView) view.findViewById(R.id.inviteBtn);
+        inviteBtn.setOnClickListener(this);
+
+        this.emailTV = (TextView) view.findViewById(R.id.emailET);
 
         return view;
     }
 
-    public void invite(View v) {
-        String email = (String) ((TextView) v.findViewById(R.id.emailET)).getText();
-        User user = null;
-        for (User u : Mockap.getUsers()){
-            if (u.getEmail().equals(email)) {
-                user = u;
-                break;
-            }
-        }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.inviteBtn:
+                String email = this.emailTV.getText().toString();
+                User user = null;
+                for (User u : Mockap.getUsers()) {
+                    if (u.getEmail().equals(email)) {
+                        user = u;
+                        break;
+                    }
+                }
 
-        if (user != null) {
-            addedUsers.add(user);
-            adapter.notifyDataSetChanged();
+                if (user != null) {
+                    addedUsers.add(user);
+                    adapter.notifyDataSetChanged();
+                }
+
         }
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.addedUsers = new ArrayList<User>();
-        this.adapter = new InvitedPassengersAdapter(getActivity(), this.addedUsers);
-        setListAdapter(adapter);
+
     }
 
 }
