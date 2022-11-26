b201cdd8896c2d79fb974ce9f5c48134ea070ce1
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-11-26 19:10:20 +0100

    changed inherited fragment class

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/AccountDetailsFragment.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/AccountDetailsFragment.java
@@ -1,17 +1,19 @@
 package com.example.uberapp_tim13.fragments;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.ListFragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.uberapp_tim13.R;
 import com.example.uberapp_tim13.adapters.ride_history.PassengerDetailsAdapter;
 
-public class AccountDetailsFragment extends ListFragment {
+/*public class AccountDetailsFragment extends ListFragment {
     public static int rideNum;
     public static AccountDetailsFragment newInstance(int r) {
         AccountDetailsFragment frag = new AccountDetailsFragment();
@@ -29,5 +31,26 @@ public class AccountDetailsFragment extends ListFragment {
 
         PassengerDetailsAdapter adapter = new PassengerDetailsAdapter(getActivity(), rideNum);
         setListAdapter(adapter);
+    }
+}*/
+
+public class AccountDetailsFragment extends DialogFragment {
+    public static int rideNum;
+    public static AccountDetailsFragment newInstance(int r) {
+        AccountDetailsFragment frag = new AccountDetailsFragment();
+        rideNum = r;
+        return frag;
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_account_details, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.list);
+        PassengerDetailsAdapter adapter = new PassengerDetailsAdapter(getActivity(), rideNum);
+        listView.setListAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
