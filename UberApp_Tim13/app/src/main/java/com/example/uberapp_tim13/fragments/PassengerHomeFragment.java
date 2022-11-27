b264416acb0cafe12b7aa1bfe9577e01bd06ef77
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-11-27 18:06:41 +0100

    changed edittext to timepicker in passenger main fragment

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/PassengerHomeFragment.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/fragments/PassengerHomeFragment.java
@@ -1,16 +1,23 @@
 package com.example.uberapp_tim13.fragments;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.uberapp_tim13.R;
 import com.example.uberapp_tim13.activities.AccountSettingsActivity;
 import com.example.uberapp_tim13.tools.FragmentTransition;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Calendar;
 
 public class PassengerHomeFragment extends Fragment {
 
@@ -29,7 +36,30 @@ public class PassengerHomeFragment extends Fragment {
                 FragmentTransition.to(RideSettingsFragment.newInstance(), getActivity(), true, R.id.passengerFL);
             }
         });
-
+        
+        setTimePicker(view);
         return view;
+
+    }
+
+    private void setTimePicker(View view) {
+        MaterialButton pickUpTimeBtn = view.findViewById(R.id.pickUpTimeBtn);
+        EditText pickUpTimeET = view.findViewById(R.id.pickUpTimeET);
+        pickUpTimeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), R.style.TimePickerTheme,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                pickUpTimeET.setText(hourOfDay + ":" + minute);
+                            }
+                        }, hour, minute, false);
+                timePickerDialog.show();
+            }
+        });
     }
 }
