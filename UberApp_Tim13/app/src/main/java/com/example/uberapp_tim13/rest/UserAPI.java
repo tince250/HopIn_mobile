08de208718dc89f7bc09b28ae9a1f793f53b086d
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-13 18:44:16 +0100

    created method for getting user by email from back in userService

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/rest/UserAPI.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/rest/UserAPI.java
@@ -4,9 +4,13 @@ import com.example.uberapp_tim13.dtos.UserDTO;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface UserAPI {
 
     @GET(RestUtils.USER_GET)
     Call<UserDTO> doGetUser();
+
+    @GET("user?")
+    Call<UserDTO> getUserByEmail(@Query("email") String email);
 }
--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/services/UserService.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/services/UserService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.example.uberapp_tim13.dtos.RideReturnedDTO;
 import com.example.uberapp_tim13.dtos.UserDTO;
 import com.example.uberapp_tim13.rest.RestUtils;
 import com.example.uberapp_tim13.rest.UserAPI;
@@ -14,54 +15,25 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class UserService extends Service {
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+public class UserService {
+   public UserDTO returnedUser;
 
-//        Intent ints = new Intent(MainActivity.SYNC_DATA);
-//        int status = ReviewerTools.getConnectivityStatus(getApplicationContext());
-//        ints.putExtra(RESULT_CODE, status);
+   public void getUserByEmail(String email) {
+       Call<UserDTO> call = RestUtils.userApi.getUserByEmail(email);
+       call.enqueue(new Callback<UserDTO>() {
+           @Override
+           public void onResponse(Call<UserDTO> call, Response<UserDTO> response) {
+               if (response.code() == 200) {
+                   Log.d("REZ", response.body().toString());
+                   returnedUser = response.body();
+               }
+           }
 
-        //ima konekcije ka netu skini sta je potrebno i sinhronizuj bazu
-//        if(status == ReviewerTools.TYPE_WIFI || status == ReviewerTools.TYPE_MOBILE){
-//            TagToSend tts = new TagToSend();
-//            tts.setName("Test tag");
-//            tts.setDateModified("2017-05-09");
-
-        /*
-         * Poziv REST servisa se odvija u pozadini i mi ne moramo da vodimo racuna o tome
-         * Samo je potrebno da registrujemo sta da se desi kada odgovor stigne od nas
-         * Taj deo treba da implementiramo dodavajuci Callback<List<Event>> unutar enqueue metode
-         *
-         * Servis koji pozivamo izgleda:
-         * http://<service_ip_adress>:<service_port>/rs.ftn.reviewer.rest/rest/proizvodi/
-         * */
-        Call<UserDTO> call = RestUtils.userApi.doGetUser();
-        call.enqueue(new Callback<UserDTO>() {
-            @Override
-            public void onResponse(Call<UserDTO> call, Response<UserDTO> response) {
-                if (response.code() == 200) {
-                    Log.d("REZ", "Meesage recieved");
-                } else {
-                    Log.d("REZ", "Meesage recieved: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<UserDTO> call, Throwable t) {
-                Log.d("REZ", t.getMessage() != null ? t.getMessage() : "error");
-            }
-        });
-
-//        sendBroadcast(ints);
-
-        stopSelf();
-
-        return START_NOT_STICKY;
-    }
-
-    @Override
-    public IBinder onBind(Intent arg0) {
-        return null;
-    }
+           @Override
+           public void onFailure(Call<UserDTO> call, Throwable t) {
+               returnedUser = null;
+               Log.d("REZ", t.getMessage() != null ? t.getMessage() : "error");
+           }
+       });
+   }
 }
