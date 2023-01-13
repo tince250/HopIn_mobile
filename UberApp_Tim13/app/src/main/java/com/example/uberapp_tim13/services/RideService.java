1a1ea74bf5e245d4ae312286b9f710db9cbe73a0
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-13 17:54:50 +0100

    created RideService with callback function for orderRide api

---
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/services/RideService.java
@@ -0,0 +1,44 @@
+package com.example.uberapp_tim13.services;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.uberapp_tim13.dtos.RideDTO;
+import com.example.uberapp_tim13.dtos.RideReturnedDTO;
+import com.example.uberapp_tim13.dtos.UserDTO;
+import com.example.uberapp_tim13.rest.RestUtils;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RideService {
+    public static RideDTO rideInCreation;
+    public static RideReturnedDTO returnedRide;
+    public static boolean success = false;
+
+    public RideService() {
+        this.rideInCreation = new RideDTO();
+    }
+
+    public void orderRide() {
+        Call<RideReturnedDTO> call = RestUtils.rideApi.orderRide(rideInCreation);
+        call.enqueue(new Callback<RideReturnedDTO>() {
+            @Override
+            public void onResponse(Call<RideReturnedDTO> call, Response<RideReturnedDTO> response) {
+                if (response.code() == 200) {
+                    Log.d("REZ", response.body().toString());
+                    returnedRide = response.body();
+                    success = true;
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RideReturnedDTO> call, Throwable t) {
+                success = false;
+
+                Log.d("REZ", t.getMessage() != null ? t.getMessage() : "error");
+            }
+        });
+    }
+}
