dcd070365972dcd6dd814f962589d2c9cf0e8862
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-14 13:12:26 +0100

    added call for ordering ride in ride service

--- UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/services/RideService.java
+++ UberApp_Tim13/app/src/main/java/com/example/uberapp_tim13/services/RideService.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.example.uberapp_tim13.dtos.CredentialsDTO;
+import com.example.uberapp_tim13.dtos.RideDTO;
 import com.example.uberapp_tim13.dtos.RideReturnedDTO;
 import com.example.uberapp_tim13.dtos.TokenDTO;
 import com.example.uberapp_tim13.rest.RestUtils;
@@ -23,16 +24,24 @@ import retrofit2.Response;
 
 public class RideService extends Service {
 
+    public static RideDTO rideInCreation = new RideDTO();
+    public static RideReturnedDTO returnedRide;
+
     ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Bundle extras = intent.getExtras();
+        String method = (String) extras.get("method");
 
         executor.execute(new Runnable() {
             @Override
             public void run() {
-                getRideById();
+                if (method.equals("getRideById")) {
+                    getRideById();
+                } else if (method.equals("orderRide")) {
+                    orderRide();
+                }
             }
         });
 
@@ -55,6 +64,23 @@ public class RideService extends Service {
                 Log.d("REZ", t.getMessage() != null ? t.getMessage() : "error");
             }
         });
+    }
+
+    public void orderRide() {
+        Call<RideReturnedDTO> call = RestUtils.rideAPI.orderRide(rideInCreation);
+        call.enqueue((new Callback<RideReturnedDTO>() {
+            @Override
+            public void onResponse(Call<RideReturnedDTO> call, Response<RideReturnedDTO> response) {
+                Log.d("REZ", response.body().toString());
+                //TODO: add broadcast sending return body if needed
+                returnedRide = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<RideReturnedDTO> call, Throwable t) {
+                Log.d("REZ", "ERROR");
+            }
+        }));
     }
 
     private void sendRideByIdBroadcast(RideReturnedDTO ride){
